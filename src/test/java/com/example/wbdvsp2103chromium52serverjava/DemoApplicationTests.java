d42ff85425c48bd237353130a8aa2000ef0831d6
Kenny Jung <dukyoung01@naver.com>
2021-01-31 10:46:32 -0500

    Create login web page

---
+++ src/test/java/com/example/wbdvsp2103chromium52serverjava/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdvsp2103chromium52serverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
